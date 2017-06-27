e8790332933334043d539c077f3298fe9eb95225
Rachel Eiting <reiting1@gmail.com>
2017-06-26 20:05:45 -0500

    updated server information

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
-    user: 'rachel',
-    password: "Byebyeoz3",
+    user: '',
+    password: "",
     database: 'Chirper'
 });
 
